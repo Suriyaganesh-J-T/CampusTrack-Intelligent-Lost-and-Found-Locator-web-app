86af7ee876391bf68875cfddd4369326de79f2b0
Suriyaganesh-J-T <suriyaganesh.jt@gmail.com>
2025-12-13 01:09:47 +0530

    Delete frontend/src/services/auth.js

--- frontend/src/services/auth.js
+++
@@ -1,9 +0,0 @@
-export function saveToken(token) {
-    localStorage.setItem("jwt", token);
-}
-export function clearToken() {
-    localStorage.removeItem("jwt");
-}
-export function getToken() {
-    return localStorage.getItem("jwt");
-}
